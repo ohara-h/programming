a33e52b98d222c23bd457d75e309b87d46eaff13
ohara-h <k151656@kansai-u.ac.jp>
2024-03-08 23:03:22 +0900

    リストページとDBに日時登録できるよう修正

---
+++ list.php
@@ -0,0 +1,43 @@
+<!doctype HTML>
+<html lang = "ja">
+    <head>
+        <meta charset="utf-8">
+        <title>アカウント一覧</title>
+        <link rel="stylesheet" type="text/css" href="list.css">
+    </head>
+
+    <body>
+        <header>
+            <div class="box1">
+                <ul class="ul1">
+                    <li class="li1">トップ</li>
+                    <li class="li3">プロフィール</li>
+                    <li class="li3">D.I.Blogについて</li>
+                    <li class="li3">登録フォーム</li>
+                    <li class="li3">問い合わせ</li>
+                    <li class="li3"><a href = "regist.php">アカウント登録</a></li>
+                    <li class="li3"><a href = "list.php">アカウント一覧</a></li>
+                    <li class="li2">その他</li>
+                </ul>
+            </div>
+        </header>
+        
+            <ul class="ul2">
+                <li class="li4">ID<br><div>あああ</div><div>いいい</div></li>
+                <li class="li4">名前(姓)<br></li>
+                <li class="li4">名前(名)<br></li>
+                <li class="li4">カナ(姓)<br></li>
+                <li class="li4">カナ(名)<br></li>
+                <li class="li4">メールアドレス<br></li>
+                <li class="li4">性別<br></li>
+                <li class="li4">アカウント権限<br></li>
+                <li class="li4">削除フラグ<br></li>
+                <li class="li4">登録日時<br></li>
+                <li class="li4">更新日時<br></li>
+                <li class="li4">操作<br></li>
+            </ul>
+        <footer>
+            <div class="box3">Copyright D.I.works| D.I.blog is the one which provides A to Z about programming</div>
+        </footer>
+    </body>
+</html>
